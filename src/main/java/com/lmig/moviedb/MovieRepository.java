b594b1ae5a78c3cf0e44bcab9e3a3310edc4c4a3
ravibhupathiraju <ravi.bhupathiraju@gmail.com>
2017-06-29 16:34:00 -0400

    adding new code from 6-29

--- src/main/java/com/lmig/moviedb/MovieRepository.java
+++ src/main/java/com/lmig/moviedb/MovieRepository.java
@@ -1,8 +1,36 @@
 package com.lmig.moviedb;
 
 import com.lmig.moviedb.Movie;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
-	
+	// @Query("SELECT m FROM Movie m WHERE m.movie = :movie")
+	//// public String find(@Param("movie") String movie);
+	// List<Movie> find(@Param("movie") String movie);
+	//// return m;
+	List<Movie> findByGenre(String genre);
+
+	List<Movie> findByMovie(String movie);
+
+	List<Movie> findByLanguage(String language);
+
+	List<Movie> findByMovieAndGenre(String movie, String genre);
+
+	List<Movie> findByGenreAndLanguage(String genre, String language);
+	// List<Movie> findByIdOrMovieOrYearOrRatingOrScoreOrGenreOrLanguage(Integer
+	// Id, String Movie, Integer Year, String Rating, Integer Score, String
+	// Genre, String Language);
+
+	@Query("SELECT m FROM Movie m WHERE m.movie LIKE CONCAT('%', :movie, '%') "
+			+ " AND (0 = :year OR m.year = :year) "
+			+ " AND ('' = :rating OR m.rating = :rating) "
+			)
+	List<Movie> search(@Param("movie") String Movie, @Param("year") Integer Year, @Param("rating") String Rating);
+//	List<Movie> search(@Param("movie") String Movie, @Param("movie") Integer Year, @Param("movie") String Rating,
+//			@Param("score") String Score, @Param("genre") String Genre, @Param("language") String Language);
 }
--- src/main/java/com/lmig/moviedb/MovieRestController.java
+++ src/main/java/com/lmig/moviedb/MovieRestController.java
@@ -1,16 +1,21 @@
 package com.lmig.moviedb;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //import com.google.gson.Gson;
@@ -79,9 +84,106 @@ public class MovieRestController {
 	public void addMovie(@RequestBody Movie movie) {
 		movieRepository.save(movie);
 
-//		personArray.add(new Person(person.getName(), "indy", "male", 5, "ravi is going to gfc"));
-//		personArray.add(new Person(personName, "indy", "male", 5, "ravi is going to gfc"));
-//		System.out.println(resultArray.toString());
-//		return personArray;
+		// personArray.add(new Person(person.getName(), "indy", "male", 5, "ravi
+		// is going to gfc"));
+		// personArray.add(new Person(personName, "indy", "male", 5, "ravi is
+		// going to gfc"));
+		// System.out.println(resultArray.toString());
+		// return personArray;
 	}
+
+	@RequestMapping(value = "/api/putMovie", method = RequestMethod.PUT)
+	public void updateMovie(@RequestBody Movie movie) {
+		movieRepository.save(movie);
+		// post man URL for putting it is http://localhost:8080/api/putMovie/.
+		// Input JSON if id is existing, it will get updated.
+
+	}
+
+	// this deletes single movie using Pathvariable
+	@RequestMapping(value = "/api/deleteMovie/{id}", method = RequestMethod.DELETE)
+	public void deleteMovie(@PathVariable(name = "id", required = true) int id) {
+		movieRepository.delete(id);
+		// post man URL for putting it is
+		// http://localhost:8080/api/deleteMovie/98.
+		// Anything in the body will get ignored
+
+	}
+
+	// this deletes single movie using delete method and data supplied thru body
+	@RequestMapping(value = "/api/deleteMovie", method = RequestMethod.DELETE)
+	public void deleteMovie(@RequestBody Integer id) {
+		movieRepository.delete(id);
+		// post man URL for putting it is
+		// http://localhost:8080/api/deleteMovie/98.
+		// Anything in the body will get ignored
+
+	}
+
+	// multiple movies deleted supplied in the form of a list from the postman
+	// body
+	// http://localhost:8080/api/deleteMovies is the URL in post man and the
+	// body is supplied
+	// in the list form as [1,2] where 1 and 2 are the IDs that we want to pass
+	// to the list
+	@RequestMapping(value = "/api/deleteMovies", method = RequestMethod.DELETE)
+	public void deleteMovie(@RequestBody List<Integer> ids) {
+		for (Iterator iterator = ids.iterator(); iterator.hasNext();) {
+			Integer integer = (Integer) iterator.next();
+			movieRepository.delete(integer);
+		}
+
+	}
+
+	// Get function using @Query.
+	@RequestMapping(value = "/api/getMovieQuery2", method = RequestMethod.GET)
+	public List<Movie> getMovieQuery(@RequestParam(defaultValue = "") String movie,
+			@RequestParam(defaultValue = "0") Integer year, @RequestParam(defaultValue = "") String rating,
+			@RequestParam(defaultValue = "") String score, @RequestParam(defaultValue = "") String genre,
+			@RequestParam(defaultValue = "") String language) {
+		// @Param("movie") String movie,@Param("year") String
+		// year,@Param("rating") String rating,@Param("score") String
+		// score,@Param("genre") String genre,@Param("language") String
+		// language) {
+		List<Movie> result = new ArrayList<>();
+		System.out.println(language + genre);
+		// result = movieRepository.findByMovie(movie);
+		// for (int i = 0; i < result.size(); i++) {
+		// Movie m = result.get(i);
+		// System.out.println(m.year);
+		// }
+		// result = movieRepository.findByLanguage(language);
+		// for (int i = 0; i < result.size(); i++) {
+		// Movie m = result.get(i);
+		// System.out.println(m.language);
+		// }
+		// return result;
+
+		// result = movieRepository.findByMovieAndGenre(movie,genre);
+		//// for (int i = 0; i < result.size(); i++) {
+		//// Movie m = result.get(i);
+		//// System.out.println(m.language);
+		//// }
+		// return result;
+
+		// result = movieRepository.findByGenreAndLanguage(genre,language);
+		// for (int i = 0; i < result.size(); i++) {
+		// Movie m = result.get(i);
+		// System.out.println(m.language);
+		// }
+		// return result;
+
+		// result =
+		// movieRepository.findByIdAndMovieAndYearAndRatingAndScoreAndGenreAndLanguage
+		// (id, movie,year,rating,score,genre,language);
+		// for (int i = 0; i < result.size(); i++) {
+		// Movie m = result.get(i);
+		// System.out.println(m.language);
+		// }
+//		result = movieRepository.search(movie, year, rating, score, genre, language);
+		result = movieRepository.search(movie, year, rating);
+		System.out.println("result" + result);
+		return result;
+	}
+
 }
